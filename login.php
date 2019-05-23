ba8c275209217bec2a98fb2fbb22121fd9d6c8d6
Brandon Benedict <brandonbenedict.biano@gmail.com>
2019-05-23 23:37:42 +0800

    Cleaned up login.php portion

--- login.php
+++ login.php
@@ -1,42 +1,35 @@
-
 <?php
-   include("config.php");
-   session_start();
-   
-   if(isset($_POST['login_user'])) {
-      // username and password sent from form 
-      
-      $myusername = $_POST['usern'];
-      $mypassword = $_POST['passw']; 
+  include("config.php");
+  session_start();
 
-      if (empty($myusername)) {
-         die("Username is required" );
-      }
+  if(isset($_POST['login_user'])) {
+    // username and password sent from form
 
-      elseif (empty($mypassword)) {
-         die("Password is required" );
-       }
-      
-      else {
-         $sql = "SELECT * FROM users WHERE username = '$myusername' and password = '$mypassword'";
-         $result = $conn->query($sql);
+    $myusername = $_POST['usern'];
+    $mypassword = $_POST['passw'];
 
-         if($result->num_rows >0){
-            while($row=$result->fetch_assoc()){
-              $_SESSION['username'] = $username;
-              $_SESSION['success'] = "You are now logged in";
-              header('location: welcome.php');
-            }
-         }
-         else {
-             header('location: index.php');
-         }
+    if(empty($myusername)) {
+      die("Username is required" );
+    }else if(empty($mypassword)) {
+      die("Password is required" );
+    }else{
+      $sql = "SELECT * FROM users WHERE username = '$myusername' and password = '$mypassword'";
+      $result = $conn->query($sql);
+
+      if($result->num_rows >0){
+        while($row=$result->fetch_assoc()){
+          $_SESSION['username'] = $username;
+          $_SESSION['success'] = "You are now logged in";
+          header('location: welcome.php');
+        }
+      }else{
+        header('location: index.php');
       }
     }
-
+  }
 ?>
 
-<!-- 
+<!--
 <html>
 <head>
   <title>Registration system PHP and MySQL</title>
@@ -46,7 +39,7 @@
   <div class="header">
    <h2>Login</h2>
   </div>
-    
+
   <form method="post" action="login.php">
    <div class="input-group">
       <label>Username</label>
