4bc7ed06d7081f5e89bc984fd7a1f2bff9ad40f7
Brandon Benedict <brandonbenedict.biano@gmail.com>
2019-05-25 00:12:16 +0800

    Changed session data to post data

--- add_user.php
+++ add_user.php
@@ -7,28 +7,28 @@
 
   // -- TEST SECTION
   // test values, remove section in actual usage
-  $_SESSION['username'] = "user8";
-  $_SESSION['password'] = "password";
-  $_SESSION['email'] = "user@email.com";
-  $_SESSION['usertype'] = "student";
+  $_POST['username'] = "user8";
+  $_POST['password'] = "password";
+  $_POST['email'] = "user@email.com";
+  $_POST['usertype'] = "student";
 
-  $_SESSION['firstname'] = "Bob";
-  $_SESSION['lastname'] = "Ong";
+  $_POST['firstname'] = "Bob";
+  $_POST['lastname'] = "Ong";
   // -- END OF TEST SECTION
 
-  if(isset($_SESSION['username']) && isset($_SESSION['password']) && isset($_SESSION['email']) && isset($_SESSION['usertype'])){
-    if(mysqli_num_rows(mysqli_query($conn, "SELECT id FROM users WHERE username = '".$_SESSION['username']."'")) == 0){
-      $insert_sql = "INSERT INTO users (username, password, email, usertype) VALUES ('".$_SESSION['username']."', '".$_SESSION['password']."', '".$_SESSION['email']."', '".$_SESSION['usertype']."')";
+  if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['email']) && isset($_POST['usertype'])){
+    if(mysqli_num_rows(mysqli_query($conn, "SELECT id FROM users WHERE username = '".$_POST['username']."'")) == 0){
+      $insert_sql = "INSERT INTO users (username, password, email, usertype) VALUES ('".$_POST['username']."', '".$_POST['password']."', '".$_POST['email']."', '".$_POST['usertype']."')";
       echo(mysqli_query($conn, $insert_sql) ? "User successfully added to the database": "Failed to update database");
 
-      $row = mysqli_fetch_array(mysqli_query($conn, "SELECT id FROM users WHERE username = '".$_SESSION['username']."'"), MYSQLI_ASSOC);
+      $row = mysqli_fetch_array(mysqli_query($conn, "SELECT id FROM users WHERE username = '".$_POST['username']."'"), MYSQLI_ASSOC);
       if($row){
-        if($_SESSION['usertype'] == "student"){
-          $student_sql = "INSERT INTO student (first_name, last_name, user_id) VALUES ('".$_SESSION['firstname']."', '".$_SESSION['lastname']."', '".$row['id']."')";
+        if($_POST['usertype'] == "student"){
+          $student_sql = "INSERT INTO student (first_name, last_name, user_id) VALUES ('".$_POST['firstname']."', '".$_POST['lastname']."', '".$row['id']."')";
           echo("<br>");
           echo(mysqli_query($conn, $student_sql) ? "Student successfully added to the database": "Failed to update database");
-        }else if($_SESSION['usertype'] == "teacher"){
-          $teacher_sql = "INSERT INTO teacher (first_name, last_name, user_id) VALUES ('".$_SESSION['firstname']."', '".$_SESSION['lastname']."', '".$row['id']."')";
+        }else if($_POST['usertype'] == "teacher"){
+          $teacher_sql = "INSERT INTO teacher (first_name, last_name, user_id) VALUES ('".$_POST['firstname']."', '".$_POST['lastname']."', '".$row['id']."')";
           echo("<br>");
           echo(mysqli_query($conn, $teacher_sql) ? "Teacher successfully added to the database": "Failed to update database");
         }
