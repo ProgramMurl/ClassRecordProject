9fb1fb18b7880c8c53b7078af7d32ebe88aa40ba
Brandon Benedict <brandonbenedict.biano@gmail.com>
2019-05-28 20:54:33 +0800

    Minor code cleanup

--- index.php
+++ index.php
@@ -32,157 +32,153 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Login</title>
-<style>
-body {font-family: Arial, Helvetica, sans-serif;}
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Login</title>
+  <style>
+    body {
+      font-family: Arial, Helvetica, sans-serif;
+    }
 
-/* Full-width input fields */
-input[type=text], input[type=password] {
-  width: 100%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid #ccc;
-  box-sizing: border-box;
-}
-.bg-img {
-  background-image: url("resources/blue.jpg");
-  min-height: 380px;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-  position: relative;
-}
+    /* Full-width input fields */
+    input[type=text], input[type=password] {
+      width: 100%;
+      padding: 12px 20px;
+      margin: 8px 0;
+      display: inline-block;
+      border: 1px solid #ccc;
+      box-sizing: border-box;
+    }
 
-/* Set a style for all buttons */
-button {
-  background-color: #004080;
-  color: white;
-  padding: 14px 20px;
-  margin: 8px 0;
-  border: none;
-  cursor: pointer;
-  width: 100%;
-}
+    .bg-img {
+      background-image: url("resources/blue.jpg");
+      min-height: 380px;
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: cover;
+      position: relative;
+    }
 
-button:hover {
-  opacity: 0.8;
-}
+    /* Set a style for all buttons */
+    button {
+      background-color: #004080;
+      color: white;
+      padding: 14px 20px;
+      margin: 8px 0;
+      border: none;
+      cursor: pointer;
+      width: 100%;
+    }
 
-/* Extra styles for the cancel button */
-.cancelbtn {
-  width: auto;
-  padding: 10px 18px;
-  background-color: #f44336;
-}
+    button:hover {
+      opacity: 0.8;
+    }
 
-/* Center the image and position the close button */
-.imgcontainer {
-  text-align: center;
-  margin: 24px 0 12px 0;
-  position: relative;
-}
+    /* Extra styles for the cancel button */
+    .cancelbtn {
+      width: auto;
+      padding: 10px 18px;
+      background-color: #f44336;
+    }
 
-img.avatar {
-  width: 40%;
-  border-radius: 50%;
-}
+    /* Center the image and position the close button */
+    .imgcontainer {
+      text-align: center;
+      margin: 24px 0 12px 0;
+      position: relative;
+    }
 
-.container {
-  padding: 16px;
-  max-width: 100%;
-}
+    img.avatar {
+      width: 40%;
+      border-radius: 50%;
+    }
 
-span.psw {
-  float: right;
-  padding-top: 16px;
-}
+    .container {
+      padding: 16px;
+      max-width: 100%;
+    }
 
-/* The Modal (background) */
-.modal {
-  display: none; /* Hidden by default */
-  position: fixed; /* Stay in place */
-  z-index: 1; /* Sit on top */
-  left: 0;
-  top: 0;
-  width: 100%; /* Full width */
-  height: 100%; /* Full height */
-  overflow: auto; /* Enable scroll if needed */
-  background-color: rgb(0,0,0); /* Fallback color */
-  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
-  padding-top: 60px;
-}
+    span.psw {
+      float: right;
+      padding-top: 16px;
+    }
 
-/* Modal Content/Box */
-.modal-content {
-  background-color: #fefefe;
-  margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
-  border: 1px solid #888;
-  width: 520px; /* Could be more or less, depending on screen size */
-  height: 620px;
-  position: relative;
-  box-sizing: border-box;
-}
+    /* The Modal (background) */
+    .modal {
+      display: none; /* Hidden by default */
+      position: fixed; /* Stay in place */
+      z-index: 1; /* Sit on top */
+      left: 0;
+      top: 0;
+      width: 100%; /* Full width */
+      height: 100%; /* Full height */
+      overflow: auto; /* Enable scroll if needed */
+      background-color: rgb(0,0,0); /* Fallback color */
+      background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+      padding-top: 60px;
+    }
+
+    /* Modal Content/Box */
+    .modal-content {
+      background-color: #fefefe;
+      margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
+      border: 1px solid #888;
+      width: 520px; /* Could be more or less, depending on screen size */
+      height: 620px;
+      position: relative;
+      box-sizing: border-box;
+    }
 
 
-/* Add Zoom Animation */
-.animate {
-  -webkit-animation: animatezoom 0.6s;
-  animation: animatezoom 0.6s
-}
+    /* Add Zoom Animation */
+    .animate {
+      -webkit-animation: animatezoom 0.6s;
+      animation: animatezoom 0.6s
+    }
 
-@-webkit-keyframes animatezoom {
-  from {-webkit-transform: scale(0)}
-  to {-webkit-transform: scale(1)}
-}
+    @-webkit-keyframes animatezoom {
+      from {-webkit-transform: scale(0)}
+      to {-webkit-transform: scale(1)}
+    }
 
-@keyframes animatezoom {
-  from {transform: scale(0)}
-  to {transform: scale(1)}
-}
+    @keyframes animatezoom {
+      from {transform: scale(0)}
+      to {transform: scale(1)}
+    }
 
-/* Change styles for span and cancel button on extra small screens */
-@media screen and (max-width: 300px) {
-  span.psw {
-     display: block;
-     float: none;
-  }
-  .cancelbtn {
-     width: 100%;
-  }
-}
-</style>
+    /* Change styles for span and cancel button on extra small screens */
+    @media screen and (max-width: 300px) {
+      span.psw {
+        display: block;
+        float: none;
+      }
+      .cancelbtn {
+        width: 100%;
+      }
+    }
+  </style>
 </head>
-
 <body class="bg-img">
-
-    <div class="modal">
-     <h2>Login</h2>
-    </div>
-
-    <form class="modal-content animate" method="post" action="index.php">
+  <div class="modal">
+    <h2>Login</h2>
+  </div>
+  <form class="modal-content animate" method="post" action="index.php">
     <div class="imgcontainer">
       <img src="resources/img_avatar2.png" alt="Avatar" class="avatar">
     </div>
-
-     <div class="container">
-        <label for="usern"><b>Username</b></label>
+    <div class="container">
+      <label for="usern"><b>Username</b></label>
       <input type="text" placeholder="Enter Username" name="username" required>
-
       <label for="passw"><b>Password</b></label>
       <input type="password" placeholder="Enter Password" name="password" required>
-
-        <button type="submit" class="btn" name="login_user">Login</button>
-     <label>
+      <button type="submit" class="btn" name="login_user">Login</button>
+      <label>
         <input type="checkbox" checked="checked" name="remember"> Remember me
       </label>
-     </div>
-     <div class="container"style="background-color:#f1f1f1">
-     <p>
-         Not yet a member? <a href="add_user.php">Sign up</a>
-     </p>
-    </form>
-
+    </div>
+    <div class="container"style="background-color:#f1f1f1">
+    <p>
+    Not yet a member? <a href="add_user.php">Sign up</a>
+    </p>
+  </form>
 </body>
 </html>
--- welcome.php
+++ welcome.php
@@ -9,9 +9,9 @@
 ?>
 <!DOCTYPE html>
 <html>
+<head>
   <title>Welcome</title>
   <meta charset="UTF-8">
-
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
   <meta content="" name="keywords">
   <meta content="" name="description">
@@ -29,66 +29,78 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
   <style>
-  body, h1,h2,h3,h4,h5,h6 {font-family: "Montserrat", sans-serif}
-  .w3-row-padding img {margin-bottom: 12px}
-  /* Set the width of the sidebar to 120px */
-  .w3-sidebar {width: 100px;background: #222;}
-  /* Add a left margin to the "page content" that matches the width of the sidebar (120px) */
-  #main {margin-left: 120px}
-  /* Remove margins from "page content" on small screens */
-  @media only screen and (max-width: 600px) {#main {margin-left: 0}}
+    body, h1,h2,h3,h4,h5,h6{
+      font-family: "Montserrat", sans-serif;
+    }
+
+    .w3-row-padding img{
+      margin-bottom: 12px;
+    }
+
+    /* Set the width of the sidebar to 120px */
+    .w3-sidebar{
+      width: 100px;
+      background: #222;
+    }
+
+    /* Add a left margin to the "page content" that matches the width of the sidebar (120px) */
+    #main{
+      margin-left: 120px;
+    }
+
+    /* Remove margins from "page content" on small screens */
+    @media only screen and (max-width: 600px) {
+      #main {
+        margin-left: 0
+      }
+    }
   </style>
-
+</head>
 <body class="w3-black">
+  <!-- Icon Bar (Sidebar - hidden on small screens) -->
+  <nav class="w3-sidebar w3-bar-block w3-small w3-hide-small w3-center">
+    <a href="#home" class="w3-bar-item w3-button w3-padding-large w3-black">
+      <i class="fa fa-home w3-large"></i>
+      <p>HOME</p>
+    </a>
+    <a href="class.php" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
+      <i class="fa fa-graduation-cap w3-large"></i>
+      <p>CLASS</p>
+    </a>
+    <a href="" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
+      <i class="fa fa-eye w3-large"></i>
+      <p>PHOTOS</p>
+    </a>
+    <a href="settings.php" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
+      <i class="fa fa-cog w3-large"></i>
+      <p>SETTINGS</p>
+    </a>
+    <a href="kill_session.php" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
+      <i class="fa fa-sign-out w3-large"></i>
+      <p>LOGOUT</p>
+    </a>
+  </nav>
 
-<!-- Icon Bar (Sidebar - hidden on small screens) -->
-<nav class="w3-sidebar w3-bar-block w3-small w3-hide-small w3-center">
-  <a href="#home" class="w3-bar-item w3-button w3-padding-large w3-black">
-    <i class="fa fa-home w3-large"></i>
-    <p>HOME</p>
-  </a>
-  <a href="class.php" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
-    <i class="fa fa-graduation-cap w3-large"></i>
-    <p>CLASS</p>
-  </a>
-  <a href="" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
-    <i class="fa fa-eye w3-large"></i>
-    <p>PHOTOS</p>
-  </a>
-  <a href="settings.php" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
-    <i class="fa fa-cog w3-large"></i>
-    <p>SETTINGS</p>
-  </a>
-  <a href="kill_session.php" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
-    <i class="fa fa-sign-out w3-large"></i>
-    <p>LOGOUT</p>
-  </a>
-</nav>
-
-<!--  Navbar on small screens (Hidden on medium and large screens)  -->
-<div class="w3-top w3-hide-large w3-hide-medium" id="myNavbar">
-  <div class="w3-bar w3-black w3-opacity w3-hover-opacity-off w3-center w3-small">
-    <a href="#home" class="w3-bar-item w3-button" style="width:25% !important">HOME</a>
-    <a href="" class="w3-bar-item w3-button" style="width:25% !important">CLASS</a>
-    <a href="" class="w3-bar-item w3-button" style="width:25% !important">PHOTOS</a>
-    <a href="" class="w3-bar-item w3-button" style="width:25% !important">SETTINGS</a>
-    <a href="kill_session.php" class="w3-bar-item w3-button"style="width:25% !important">
-    LOGOUT</a>
+  <!--  Navbar on small screens (Hidden on medium and large screens)  -->
+  <div class="w3-top w3-hide-large w3-hide-medium" id="myNavbar">
+    <div class="w3-bar w3-black w3-opacity w3-hover-opacity-off w3-center w3-small">
+      <a href="#home" class="w3-bar-item w3-button" style="width:25% !important">HOME</a>
+      <a href="class.php" class="w3-bar-item w3-button" style="width:25% !important">CLASS</a>
+      <a href="" class="w3-bar-item w3-button" style="width:25% !important">PHOTOS</a>
+      <a href="settings.php" class="w3-bar-item w3-button" style="width:25% !important">SETTINGS</a>
+      <a href="kill_session.php" class="w3-bar-item w3-button"style="width:25% !important">LOGOUT</a>
+    </div>
   </div>
-</div>
 
-<!-- Page Content -->
-<div class="w3-padding-large" id="main">
-  <!-- Header/Home -->
-  <header class="w3-container w3-padding-32 w3-center w3-black" id="home">
-    <br>
-    <h1 class="w3-jumbo"><span class="w3-hide-small"></span> Welcome back!</h1>
-    <img src="resources/space.png" alt="space" class="w3-image" width="1002" height="1208">
-  </header>
-
-<!-- END PAGE CONTENT -->
-</div>
-
-
+  <!-- Page Content -->
+  <div class="w3-padding-large" id="main">
+    <!-- Header/Home -->
+    <header class="w3-container w3-padding-32 w3-center w3-black" id="home">
+      <br>
+      <h1 class="w3-jumbo"><span class="w3-hide-small"></span> Welcome back!</h1>
+      <img src="resources/space.png" alt="space" class="w3-image" width="1002" height="1208">
+    </header>
+  </div>
+  <!-- END PAGE CONTENT -->
 </body>
 </html>
